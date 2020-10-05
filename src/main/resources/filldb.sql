5d5f897a5d286fd61c9215a7252c5c480d5a2c6d
KarlMilann <karl-milann-henri.lobir@hei.yncrea.fr>
2020-10-05 05:42:48 +0200

    add random entries for dataset

--- src/main/resources/filldb.sql
+++ src/main/resources/filldb.sql
@@ -18,9 +18,14 @@ INSERT INTO user_roles (user_id, role_id)
 VALUES (14, 2);
 Insert into sales (id, product_name, product_profit, date, user)
 Values (1,'mansion', 12000, '2019-09-24', 14);
-Insert into sales (id, product_name, product_profit, date, user)
-Values (2, 'rabbit', 12, '2019-05-12', 15);
-Insert into sales (id, product_name, product_profit, date, user)
-Values (3,'omaewamo shinderu', 100000, '2019-06-17',16);
+
 Insert into sales (id, product_name, product_profit, date, user)
 Values (4, '50 shades of grey', 30000000, '2020-03-05',15);
+
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (2, 'rabbit20', 1232567, '2019-05-12', 15);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (3, 'omaewamo shinderu', 100000, '2019-06-17', 16);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (5, 'sdfgh', 12345345, '2020-10-23', 15);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (7, 'tezsetsfsd', 135464, '2020-10-30', 15);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (9, 'cvdfvdvd', 453453625, '2020-10-07', 15);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (11, 'lkjhgfd', 213453, '2020-10-31', 15);
+INSERT INTO mainsys_challdb.sales (id, product_name, product_profit, date, user) VALUES (13, 'teststdgfg', 12345345, '2020-10-31', 15);
